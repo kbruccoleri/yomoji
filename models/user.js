4786a010d072ade8b1ed0bc3de6c4c2ab4ffc049
HeroProtagonist <jstepper91@gmail.com>
2019-12-16 00:18:38 -0500

    Functions to query for sent and received events

--- models/user.js
+++ models/user.js
@@ -1,12 +1,26 @@
 const User = knex => ({
   async findOrCreate(username) {
     const userObject = { user_name: username }
-    let [user] = await knex('user').where(userObject)
+    let [ user ] = await knex('user').where(userObject)
 
     if (!user) {
-      [user] = await knex('user').insert(userObject, ['id'])
+      [ user ] = await knex('user').insert(userObject, ['id'])
     }
     return user
+  },
+  async getSent(user) {
+    const { id } = await this.findOrCreate(user)
+
+    const [ val ] = await knex('user_event').count().where({ from_id: id })
+
+    return val.count
+  },
+  async getReceived(user) {
+    const { id } = await this.findOrCreate(user)
+
+    const [ val ] = await knex('user_event').where({ to_id: id }).count()
+
+    return val.count
   },
 })
 
