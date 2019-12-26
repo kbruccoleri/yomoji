ec89bb8cf251f3f76ff09185985f2cf02510654c
HeroProtagonist <jstepper91@gmail.com>
2019-12-26 00:24:32 -0500

    Initial work on leaderboard function

--- knexfile.js
+++ knexfile.js
@@ -9,9 +9,9 @@ module.exports = {
   staging: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: 'yomoji_dev',
+      user:     '',
+      password: ''
     },
     pool: {
       min: 2,
--- models/index.js
+++ models/index.js
@@ -1,23 +1,36 @@
 const config = require('../knexfile')
-const knex = require('knex')(config.development)
+const knex = require('knex')(config.staging)
 
 const User = require('./user')(knex)
 const Event = require('./event')(knex)
 
-const UserEvent = knex => ({
+const UserEvent = {
   async create({ to, from, type }) {
     // todo: transaction
     const { id: to_id } = await User.findOrCreate(to)
     const { id: from_id } = await User.findOrCreate(from)
     const { id: event_id } = await Event.findOrCreate(type)
 
-    const UserEvent = await knex('event').insert({
+    const userEvent = await knex('user_event').insert({
       from_id,
       to_id,
       event_id,
     })
-    return UserEvent
+    return userEvent
+  },
+  async getLeaders() {
+    const users = await knex('user').select()
+
+    // use promisify.all
+    const userCounts = []
+    for (const user of users) {
+      const count  = await knex('user_event').where({ to_id: user.id }).count().first()
+      userCounts.push({ ...user, ...count })
+    }
+
+    return userCounts
   }
-})
+
+}
 
 module.exports = UserEvent;
