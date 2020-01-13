8af80398e17157a7a352f2a51b29192e4275b8c5
HeroProtagonist <jstepper91@gmail.com>
2020-01-12 19:30:36 -0500

    Remove sqlite3

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const UserEvent = require('../models')
+const UserEvent = require('./models')
 const { postMessage } = require('./slack')
 
 const {
