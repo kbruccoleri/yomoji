58d5059b5cf0d011ee7084b0ad2c4f56091c81e9
HeroProtagonist <jstepper91@gmail.com>
2020-01-12 19:41:41 -0500

    Run production using forever process manager

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('./app')
 
-app.listen(8080, () => {
-    console.log('Listening on 8080..')
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}...`)
 })
