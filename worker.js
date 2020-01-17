fab0e5562fe626df30da953419e4efac22d5922c
HeroProtagonist <jstepper91@gmail.com>
2020-01-17 09:47:00 -0500

    Add daily reset of limit

--- worker.js
+++ worker.js
@@ -1,17 +1,27 @@
+const { CronJob } = require('cron')
 const config = require('./knexfile')
 
 const ENV = process.env.NODE_ENV === 'production' ? 'production' : 'development'
-const knex = require('knex')(config[ENV])
 
 const worker = () => ({
     async refillUsers (limit = 5) {
+        const knex = require('knex')(config[ENV])
         try {
             await knex('user').update({ limit }).where('limit', '<', limit)
         } catch (e) {
             console.log('Error refilling users limit: ', e)
         }
         knex.destroy()
+    },
+    midnightReset() {
+        const job = new CronJob('00 00 00 * * *', async function() {
+            console.log('Refill job started at: ', new Date())
+            await this.refillUsers()
+            console.log('Refill job finished at: ', new Date())
+        })
+        job.start()
     }
 })
 
-module.exports = worker()
+const w = worker()
+w.midnightReset()
