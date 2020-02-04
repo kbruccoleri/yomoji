b520f481194b5378a26577833ada076636fe6191
Kevin Bruccoleri <kevinbruccoleri@gmail.com>
2020-02-04 14:00:52 -0500

    Fun messages to recipient

--- app/helpers/message_participants.js
+++ app/helpers/message_participants.js
@@ -8,17 +8,56 @@ const messageParticipants = (params) => {
 }
 
 const messageRecipient = ({ user, recipient, given }) => {
+    const singularTacoMessages = [
+        `<@${user}> told me to give you this:`,
+    ];
+    const pluralTacoMessages = [
+        `<@${user}> told me to give you these:`,
+        `Is it hot in here or is it that salsa on those tacos from <@${user}>?`
+    ];
+    const numberSpecificMessages = {
+        2: [
+            `Roses are red, violets are blue, you must really be killin' it, because <@${user}> sent you two!`
+        ],
+        3: [
+            `Hope you're ready for a snack because <@${user}> just sent you three!`
+        ],
+        4: [
+            `<@${user}> thinks you've knocked it out of the park and straight into a taco stand`,
+        ],
+        5: [
+            `Hot diggity dog! <@${user}> thinks you're cooler than a box of frozen empanadas`,
+            `Did it hurt? When you fell from heaven and landed on all the tacos you've been gifted by <@${user}>?`,
+        ]
+    };
+
+    let messagePool = [];
+    if (given === 1) messagePool = messagePool.concat(singularTacoMessages);
+    if (given > 1) messagePool = messagePool.concat(pluralTacoMessages);
+    if (numberSpecificMessages[given]) messagePool = messagePool.concat(numberSpecificMessages[given]);
+
+    const message = messagePool[Math.floor(Math.random() * messagePool.length)];
+
     return postMessage({
-        text: `<@${user}> gave you ${given}`,
+        text: message + ' ' + countTacos(given),
         channel: recipient
     })
 }
 
 const messageUser = ({ user, recipient, given, remaining }) => {
     return postMessage({
-        text: `You gave <@${recipient}> ${given}. You have ${remaining} left today`,
+        text: `You gave <@${recipient}> ${given} tacos. ${countTacos(given)}\n` +
+            `You have ${remaining} left today.`,
         channel: user
     })
+}
+
+const countTacos = count => {
+    const tacos = [];
+    for (let i = 0; i < count; i++) {
+        tacos.push(':taco:');
+    }
+    return tacos.join(' ');
 }
 
 module.exports = messageParticipants
